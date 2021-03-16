c33279c5d2ad1fcf5e435509e220aa8b9b45c2ce
GonzaloVG <gonzaloverdugog@gmail.com>
2021-03-16 16:55:15 +0100

    inicio pruebas

--- pruebas/teclado.js
+++ pruebas/teclado.js
@@ -2,7 +2,19 @@
 
 function empezarResolucion(){
 
-    let texto = document.getElementById("texto");
+    let texto = document.getElementById("texto").innerHTML;
+
+    let textoFragmentado = texto.split("");
+    let cursorPosition = 0;
+
+    console.log("Texto fragmentado: " +textoFragmentado);
     
+    console.log("Texto " + texto);
+
+
+}
+
+
+function comprobarPulsacion(teclaPorPulsar, teclaPulsada){
 
 }
