75e88af2a8ee525d168036f0f880f10912e5f295
Gonzalo Verdugo Garrido <gverdugo@alumnos.imf.com>
2021-03-17 14:17:45 +0100

    objeto ratio

--- pruebas/teclado.js
+++ pruebas/teclado.js
@@ -32,22 +32,45 @@ let ratioAciertoFallo = [
   ",",
   ";",
   ".",
+  "!"
 ];
 
 /**
  * !Generador carácteres
-// let ratioAciertoFallo = [];
-// for (var i = 32; i <= 255; i++) {
-//   console.log(String.fromCharCode(i));
-//   ratioAciertoFallo.push(String.fromCharCode(i));
-// }
+
 // console.log(ratioAciertoFallo);
 
  */
 
-ratioAciertoFallo.forEach((letra) => {
-  ratioAciertoFallo[letra] = [0, 0];
-});
+// let ratioAciertoFallo = [];
+let jsonLetras = "{";
+// for (var i = 32; i <= 255; i++) {
+// console.log(String.fromCharCode(i));
+// ratioAciertoFallo.push(String.fromCharCode(i));
+//   let caracter = String.fromCharCode(i);
+
+//   jsonLetras += '"' + caracter + '": {"aciertos":' + 0 + ',"fallos": ' + 0 + '}';
+//   if (i < 255) {
+//     jsonLetras += ",";
+//   }
+
+
+
+
+
+
+for (let i = 0; i < ratioAciertoFallo.length; i++) {
+  jsonLetras += '"' + ratioAciertoFallo[i] + '": {"aciertos":' + 0 + ',"fallos": ' + 0 + '}';
+  if (i < ratioAciertoFallo.length - 1) {
+    jsonLetras += ",";
+  }
+
+}
+jsonLetras += "}";
+
+
+
+
 
 function prepararResolucion() {
   let texto = document.getElementById("texto");
@@ -78,44 +101,56 @@ function empezarResolucion(texto, caracteres) {
    * * Si el resultado es verdadero, sumara un acierto y el cursor avanzara a la proxima posicion
    * * Si es falso, sumara una fallo
    */
-
+  let resolucionCompleta = false;
   let fallosTotales = 0;
   let exitosTotales = 0;
   let spansArray = texto.getElementsByTagName("span");
 
-  // texto[cursorPosition].className = "cursor";
 
-  console.log();
 
   /**
-   * !INTERVALO DE TIEMPO EN EL QUE EL CURSOR PARPADEA
+   * *INTERVALO DE TIEMPO EN EL QUE EL CURSOR PARPADEA
    */
-  setInterval(() => {
-    spansArray[cursorPosition].className = "cursorActive";
-    setTimeout(() => {
-      spansArray[cursorPosition].className = "cursorDesactive";
-    }, 500);
-  }, 1000);
+  if (!resolucionCompleta) {
+    setInterval(() => {
+
+      spansArray[cursorPosition].className = "cursorActive";
+      setTimeout(() => {
+        spansArray[cursorPosition].className = "cursorDesactive";
+      }, 500);
+    }, 1000);
+  }
+  console.log(jsonLetras);
+  jsonLetras = JSON.parse(jsonLetras);
+  console.log(caracteres);
+
+  console.log(typeof cursorPosition[cursorPosition]);
+
+
   window.addEventListener("keypress", (event) => {
     if (comprobarPulsacion(caracteres[cursorPosition], event.key)) {
       exitosTotales++;
-      console.log(
-        ratioAciertoFallo[caracteres[cursorPosition].toLowerCase()][0]
-      );
+
+
 
       // * SUMAR UN ACIERTO EN LA LETRA QUE EL CURSOR SEÑALA
-      ratioAciertoFallo[caracteres[cursorPosition].toLowerCase()][0]++;
-      console.log(ratioAciertoFallo);
-      // ratioAciertoFallo[caracteres[cursorPosition]][0]++;
 
-      // console.log("exito " + exito);
-      // console.log("fallo " + fallo);
-      // console.log(
-      //   "tecla que tenias que pulsar " +
-      //     caracteres[cursorPosition] +
-      //     " tecla que has pulsado " +
-      //     event.key
-      // );
+      let caracterEnfocado = caracteres[cursorPosition];
+
+      jsonLetras.a.aciertos++;
+
+      jsonLetras.caracterEnfocado.aciertos++;
+
+      console.log(caracterEnfocado);
+      // console.log(typeof jsonLetras.a);
+
+
+
+
+
+      // console.log(ratioAciertoFallo);
+
+
 
       // * CAMBIAR EL COLOR DE LA LETRA (VERDE)
       spansArray[cursorPosition].style.color = "green";
@@ -125,22 +160,25 @@ function empezarResolucion(texto, caracteres) {
       fallosTotales++;
 
       // * SUMAR UN ERROR EN LA LETRA QUE EL CURSOR SEÑALA
-      ratioAciertoFallo[caracteres[cursorPosition].toLowerCase()][1]++;
+      // ratioAciertoFallo[caracteres[cursorPosition].toLowerCase()][1]++;
+      // jsonLetras.caracteres[cursorPosition].fallos++;
+      console.log(jsonLetras.caracteres[cursorPosition]);
+      // console.log(ratioAciertoFallo[caracteres]);
 
-      console.log(ratioAciertoFallo);
-      // ratioAciertoFallo[spansArray[cursorPosition]][1]++;
-      // console.log("exito " + exito);
-      // console.log("fallo " + fallo);
-      // console.log(
-      //   "tecla que tenias que pulsar " +
-      //     caracteres[cursorPosition] +
-      //     " tecla que has pulsado " +
-      //     event.key
-      // );
       // * CAMBIAR EL COLOR DE LA LETRA (ROJO)
       spansArray[cursorPosition].className = "cursorDesactive";
       spansArray[cursorPosition].style.color = "red";
     }
+
+
+    if (cursorPosition == caracteres.length - 2) {
+      resolucionCompleta = true;
+      mostrarStatsResolucion(exitosTotales, fallosTotales, 0, ratioAciertoFallo);
+
+    }
+
+
+
   });
 }
 
@@ -158,6 +196,21 @@ function comprobarPulsacion(teclaPorPulsar, teclaPulsada) {
     return false;
   }
 }
+
+
+function mostrarStatsResolucion(aciertos, fallos, fallosPorTecla) {
+
+
+  console.log("Aciertos: " + (aciertos - fallos));
+  console.log("Fallos: " + fallos);
+  // console.log("Tiempo de resolución: " + tiempo);
+  console.log(fallosPorTecla);
+
+
+}
+
+
+
 
 function pretty(txt, elemento) {
   console.log("//######################################");
