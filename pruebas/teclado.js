d66a7bea2cb1ca56eabc94c1c5297756ce45ed88
GonzaloVG <gonzaloverdugog@gmail.com>
2021-03-17 09:43:01 +0100

    registrador de fallos

--- pruebas/teclado.js
+++ pruebas/teclado.js
@@ -1,5 +1,53 @@
 let cursorPosition = 0;
 
+let ratioAciertoFallo = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "ñ",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  " ",
+  ",",
+  ";",
+];
+
+/**
+ * !Generador carácteres
+// let ratioAciertoFallo = [];
+// for (var i = 32; i <= 255; i++) {
+//   console.log(String.fromCharCode(i));
+//   ratioAciertoFallo.push(String.fromCharCode(i));
+// }
+// console.log(ratioAciertoFallo);
+
+ */
+
+ratioAciertoFallo.forEach((letra) => {
+  ratioAciertoFallo[letra] = [0, 0];
+});
+
 function prepararResolucion() {
   let texto = document.getElementById("texto");
 
@@ -10,10 +58,10 @@ function prepararResolucion() {
   let textoEnSpan = "";
 
   textoFragmentado.forEach((letra) => {
-    console.log(letra);
+    // console.log(letra);
     textoEnSpan += "<span>" + letra + "</span>";
   });
-  console.log(textoEnSpan);
+  // console.log(textoEnSpan);
 
   texto.innerHTML = textoEnSpan;
 
@@ -25,45 +73,57 @@ function empezarResolucion(texto, caracteres) {
 
   /**
    * ! REGISTRADOR DE PULSACIONES EXITO/FALLO
-   * 
-  * * Ejecutar la funcion comprobarPulsacion() cada vez que el usuario pulse alguna tecla
-  * * Si el resultado es verdadero, sumara un acierto y el cursor avanzara a la proxima posicion
-  * * Si es falso, sumara una fallo
-
-  */
+   * * Ejecutar la funcion comprobarPulsacion() cada vez que el usuario pulse alguna tecla
+   * * Si el resultado es verdadero, sumara un acierto y el cursor avanzara a la proxima posicion
+   * * Si es falso, sumara una fallo
+   */
 
   let fallo = 0;
   let exito = 0;
+  let spansArray = texto.getElementsByTagName("span");
   window.addEventListener("keypress", (event) => {
     if (comprobarPulsacion(caracteres[cursorPosition], event.key)) {
       exito++;
-
-      console.log("exito " + exito);
-      console.log("fallo " + fallo);
       console.log(
-        "tecla que tenias que pulsar " +
-          caracteres[cursorPosition] +
-          " tecla que has pulsado " +
-          event.key
+        ratioAciertoFallo[caracteres[cursorPosition].toLowerCase()][0]
       );
 
+      // ? SUMAR UN ACIERTO EN LA LETRA QUE EL CURSOR SEÑALA
+      ratioAciertoFallo[caracteres[cursorPosition].toLowerCase()][0]++;
+      console.log(ratioAciertoFallo);
+      // ratioAciertoFallo[caracteres[cursorPosition]][0]++;
+
+      // console.log("exito " + exito);
+      // console.log("fallo " + fallo);
+      // console.log(
+      //   "tecla que tenias que pulsar " +
+      //     caracteres[cursorPosition] +
+      //     " tecla que has pulsado " +
+      //     event.key
+      // );
+
       // ? CAMBIAR EL COLOR DE LA LETRA (VERDE)
-      let spansArray = texto.getElementsByTagName("span");
+
       spansArray[cursorPosition].style.color = "green";
 
       cursorPosition++;
     } else {
       fallo++;
-      console.log("exito " + exito);
-      console.log("fallo " + fallo);
-      console.log(
-        "tecla que tenias que pulsar " +
-          caracteres[cursorPosition] +
-          " tecla que has pulsado " +
-          event.key
-      );
+
+      // ? SUMAR UN ERROR EN LA LETRA QUE EL CURSOR SEÑALA
+      ratioAciertoFallo[caracteres[cursorPosition].toLowerCase()][1]++;
+
+      console.log(ratioAciertoFallo);
+      // ratioAciertoFallo[spansArray[cursorPosition]][1]++;
+      // console.log("exito " + exito);
+      // console.log("fallo " + fallo);
+      // console.log(
+      //   "tecla que tenias que pulsar " +
+      //     caracteres[cursorPosition] +
+      //     " tecla que has pulsado " +
+      //     event.key
+      // );
       // ? CAMBIAR EL COLOR DE LA LETRA (ROJO)
-      let spansArray = texto.getElementsByTagName("span");
       spansArray[cursorPosition].style.color = "red";
     }
   });
